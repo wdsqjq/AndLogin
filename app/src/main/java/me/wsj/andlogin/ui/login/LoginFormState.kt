1c93d22a32a7d974fb0ae9eb32f497091a76b60d
shiju.wang <shiju.wang@wheatek.com>
2021-05-08 18:45:12 +0800

    init code

---
+++ app/src/main/java/me/wsj/andlogin/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package me.wsj.andlogin.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
---
+++ article.md
@@ -0,0 +1,13 @@
+# AndLogin
+
+
+
+
+
+
+
+参考：
+
+https://github.com/Xiasm/LoginArchitecture
+
+https://blog.csdn.net/weixin_33670786/article/details/88008191
